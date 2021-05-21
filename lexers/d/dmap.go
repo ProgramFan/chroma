925e8e6dfefcc272bd3eaaa93ba698d5a31a3eb6
Yang Zhang <yang_zhang@iapcm.ac.cn>
2021-05-22 00:54:59 +0800

    Implement a stateless dmap lexer

--- lexers/d/dmap.go
+++ lexers/d/dmap.go
@@ -8,36 +8,54 @@ import (
 // Dmap lexer.
 var Dmap = internal.Register(MustNewLazyLexer(
 	&Config{
-		Name:      "Dmap",
-		Aliases:   []string{"dmap"},
-		Filenames: []string{"*.dmap"},
-		MimeTypes: []string{"text/x-dmap"},
+		Name:            "Dmap",
+		Aliases:         []string{"dmap"},
+		Filenames:       []string{"*.dmap"},
+		MimeTypes:       []string{"text/x-dmap"},
+		NotMultiline:    false,
+		DotAll:          false,
 		CaseInsensitive: true,
 	},
 	dmapRules,
 ))
 
-// NOTE: The dmap grammar is defined in NX-NASTRAN dmap manual. This lexer is
-// preliminary and can only recoganize comments and strings. More complex
-// grammars can be added.
 func dmapRules() Rules {
 	return Rules{
-		// Dmap distinguishes lines, so we first chop lines. This is enabled
-		// together by requiring multiline regex mode.
+		// Since the dmap language is a command-driven language, there is no
+		// deliminators for functions and instructions and data. So we need just one
+		// state. The tricky details here is to arrange the patterns in a correct
+		// order so that the most specific pattern goes first.
 		"root": {
-			{`^[ ]*\n`, Text, nil},
-			{`^\$.*\n`, Comment, nil},
-			{`^(.{72})(.+\n)`, ByGroups(UsingSelf("line"), Comment), nil},
-			{`^.+\n`, UsingSelf("line"), nil},
+			Include("whitespace"),
+			Include("keywords"),
+			Include("literials"),
+			{`[a-z0-9]+`, Name, nil},
+			Include("operators"),
+			{`[(),/]`, Punctuation, nil},
+			{`.`, Text, nil},
 		},
-		"line": {
-			{`([^$]+)(\$.+\n?)`, ByGroups(UsingSelf("statement"), Comment), nil},
-			{`([^$]+)(\$\n)`, ByGroups(UsingSelf("statement"), Text), nil},
-			{`(.+)(\n?)`, ByGroups(UsingSelf("statement"), Text), nil},
+		"whitespace": {
+			{`^\$\n`, Comment, nil},
+			{`\$.+\n`, Comment, nil},
+			{`\$\n`, Text, nil},
+			{`\s+`, Text, nil},
 		},
-		"statement": {
-			{`(.*)('.*?')(.*)`, ByGroups(Text, LiteralString, Text), nil},
-			{`.*`, Text, nil},
+		"keywords": {
+			{Words(`\b`, `\b`, "and", "or", "not", "xor", "eqv"), Operator, nil},
+			{Words(`\b`, `\b`, "always", "never", "true", "false", "nogo"), KeywordConstant, nil},
+			{Words(`\b`, `\b`, "if", "then", "else", "endif", "do", "while", "enddo",
+				"subdmap", "call", "return", "exit", "end", "jump", "label"), Keyword, nil},
+			{Words(`\b`, `\b`, "type", "dbview", "file", "dbequiv", "dbdelete"), KeywordReserved, nil},
+		},
+		"literals": {
+			{`[+-]?\d+\.(\d+)?([de]\d+)?`, LiteralNumberFloat, nil},
+			{`[+-]?\d+`, LiteralNumberInteger, nil},
+			{`'.+?'`, LiteralString, nil},
+		},
+		"operators": {
+			{`([*]{2}|[*]|\+|-|/)`, Operator, nil},
+			{`(<>|<=|><|>=|<|>|=)`, Operator, nil},
+			{`&`, Operator, nil},
 		},
 	}
 }
